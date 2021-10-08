ec07c922c05230c730c844254974efd0ea4d2170
Alzound <61034726+Alzound@users.noreply.github.com>
2021-10-08 17:14:07 -0500

    #2 entrega

---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    [SerializeField]
+    public List<string> textRight = new List<string>();
+    [SerializeField]
+    public List<string> textLeft = new List<string>();
+    [SerializeField]
+    public List<string> textMain = new List<string>();
+
+    public TextMeshProUGUI mainText;
+
+    int indexL = 0, indexR = 0, indexM = 0; 
+
+    private void Start()
+    {
+        indexL = 0;
+        indexR = 0;
+        indexM = 0;
+    }
+
+
+    public void MainText()
+    {
+        
+        mainText.text = textMain[indexM];
+        indexM++; 
+    }
+
+    public void RightText()
+    {
+        mainText.text = textRight[indexR];
+        indexR++;
+    }
+
+    public void LeftText()
+    {
+        mainText.text = textLeft[indexL];
+        indexL++;
+    }
+}
