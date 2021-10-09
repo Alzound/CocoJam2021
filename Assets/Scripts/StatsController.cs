74a0e2df65e3b87deb832a58b0e36dbc076c926d
VicPina <vicpanzu7@gmail.com>
2021-10-08 23:35:13 -0500

    Stat scene reupload

---
+++ Assets/Scripts/StatsController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsController : MonoBehaviour
+{
+    public int availablePoints, bravePoints, nervePoints, luckPoints;
+    public List<Button> arrows = new List<Button>();
+
+    public void Add()
+    {
+        
+    }
+}
